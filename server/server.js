f8a8c54ee10b306dd0f114b412b3a836e3914082
Shivansh Yadav <shivanshyadav50@gmail.com>
2025-07-17 15:38:14 -0400

    Update server.js
    added client url from env

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.options('*', cors());
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.CLIENT_URL,
     methods: ['GET', 'POST', 'DELETE', 'PUT'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
